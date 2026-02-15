3f583ef86f85db73f7273a7f5cb3c5a374ea3ace
sgking <kopanev.miner-s@yandex.ru>
2026-02-15 16:31:34 +0500

    Refactor: build nested title dictionary with seasons and episodes

--- main.py
+++ main.py
@@ -26,40 +26,77 @@ def get_episodes(path, extensions=("mp4", "avi", "mkv")):
     return natural_sort(video_files)
 
 
+def find_subdir(dir_list):
+    finded_dirs = []
+    for dir in dir_list:
+        sub_dirs = []
+        for sub_dir in os.listdir(dir):
+            path = os.path.join(dir, sub_dir)
+            if os.path.isdir(path):
+                sub_dirs.append(path)
+        finded_dirs.append(sub_dirs)
+    return finded_dirs
+
+
 title_list = []
-for dir0 in os.listdir(config.main_dir):
-    path_level1 = os.path.join(config.main_dir, dir0)
-    if os.path.isdir(path_level1):
-        title_list.append(path_level1)
-        for dir1 in os.listdir(path_level1):
-            path_level2 = os.path.join(path_level1, dir1)
-            if os.path.isdir(path_level2):
-                title_list.append(path_level2)
+for name in os.listdir(config.main_dir):
+    path = os.path.join(config.main_dir, name)
+
+    if os.path.isdir(path):
+        title_list.append(path)
+
 title_list = natural_sort(title_list)
+
+sub_dirs = find_subdir(title_list)
+title_list_with_subs = []
+
+for i in range(len(title_list)):
+    title = title_list[i]
+    subs = sub_dirs[i]
+    title_list_with_subs.append([title] + subs)
 
 
 title_dict = {}
 
-root_episodes = get_episodes(config.main_dir)
-if root_episodes:
-    title_dict[config.main_dir] = root_episodes
+for dir_group in title_list_with_subs:
+    cur = dir_group[0]
+    title_dict[cur] = {}
 
-for title in title_list:
-    episodes = get_episodes(title)
-    if episodes:
-        title_dict[title] = episodes
+    for sub_dir in dir_group[1:]:
+        episodes = get_episodes(sub_dir)
+        title_dict[cur][os.path.basename(sub_dir)] = episodes
 
+    episodes = get_episodes(cur)
+    title_dict[cur]["root"] = episodes
 
-playlist_files = []
-while title_dict:
-    cur_title = random.choice(list(title_dict.keys()))
-    ep = title_dict[cur_title].pop(0)
-    playlist_files.append(ep)
-
-    if not title_dict[cur_title]:
-        del title_dict[cur_title]
+for t, seasons in title_dict.items():
+    print(t, "\n", seasons)
 
 
-print(playlist_files)
+# title_dict = {}
+
+# root_episodes = get_episodes(config.main_dir)
+# if root_episodes:
+#     title_dict[config.main_dir] = root_episodes
+
+# for title in title_list:
+#     episodes = get_episodes(title)
+#     if episodes:
+#         title_dict[title] = episodes
+
+# print(title_dict)
+
+# playlist_files = []
+# while title_dict:
+#     cur_title = random.choice(list(title_dict.keys()))
+#     ep = title_dict[cur_title].pop(0)
+#     playlist_files.append(ep)
+
+#     if not title_dict[cur_title]:
+#         del title_dict[cur_title]
+
+
+# for i in playlist_files:
+#     print(i)
 
 # subprocess.Popen(["vlc", "--play-and-exit"] + playlist_files)
